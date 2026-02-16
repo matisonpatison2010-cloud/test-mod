9b3a6e4a42a254b7dfc68eac9cf56b4f8f7aad41
matisonpatison2010-cloud <matisonpatison2010@gmail.com>
2026-02-16 15:59:39 +0100

    Update and rename ExampleMod.java to BossMusicMod.java

---
+++ src/main/java/com/example/BossMusicMod.java
@@ -0,0 +1,13 @@
+package com.example.bossmusic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BossMusicMod implements ModInitializer {
+
+    public static final String MOD_ID = "bossmusic";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Boss Music Mod Loaded!");
+    }
+}
